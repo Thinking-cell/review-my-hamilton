d05c16a1ec1339ed59da08d1519e158757a8b24d
Ranvir Singh <ranvir.singh92@yahoo.com>
2021-04-15 17:41:52 -0400

    db change

--- php/connect.php
+++ php/connect.php
@@ -6,12 +6,5 @@
  * 
  * 
  */
-try {
-    $dbh = new PDO(
-        "mysql:host=localhost;dbname=review_city_hamilton",
-        "root",
-        ""// for unix root= 000819787, password=dob
-    );
-} catch (Exception $e)  {
-    die("ERROR: Couldn't connect. {$e->getMessage()}");
-}
+
+ 
