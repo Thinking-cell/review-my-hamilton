7e4a0d2be4f60e0ad30b4fd752f4d20e49c4ff1d
Ranvir Singh <ranvir.singh92@yahoo.com>
2021-04-18 04:32:53 -0400

    Update index.php

--- index.php
+++ index.php
@@ -38,7 +38,7 @@ include "php/connect.php";
 
     <!--Adding jquery functionality ,adding api key for google, adding json data, ????Bootstrap????customization-->
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
-    <script src="https://maps.googleapis.com/maps/api/js?AIzaSyAZ7AE03g1ycTYuQoSqd_Tbd74fgIYLl_w&callback=initMap&libraries=&v=weekly" defer></script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAZ7AE03g1ycTYuQoSqd_Tbd74fgIYLl_w&callback=initMap&libraries=&v=weekly" defer></script>
     
     <script src="js/data.js"></script>
     <script src="js/script.js"></script>
